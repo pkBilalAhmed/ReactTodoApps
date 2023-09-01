54b77808b23563169a1684f2c331256bff0cb656
Bilal Ahmed <krnc92love@gmail.com>
2023-09-01 15:47:59 +0500

    git data push at origin main resolved with command "git remote set-url origin Https"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import Tasklist from "./Tasklist";
+
 
 const App = () => {
   return <div>
-    <Tasklist />
+    <h2>there was a problem to push data on orign main at github so with the help of chatgpt i resolved this making set orign url using thes command</h2>
+    <h3>git remote -v</h3>
+    <h3>git remote set-url origin https://github.com/yourusername/your-repo.git</h3>
+    <h3>git push --set-upstream origin main</h3>
   </div>;
 };
 
