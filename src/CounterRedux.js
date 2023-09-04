832007bb68fc110c4a89bbbaa00c60dd90e6e9a6
Bilal Ahmed <krnc92love@gmail.com>
2023-09-04 21:43:55 +0500

    Counter component added for Redux Counter app at initial state

---
+++ src/CounterRedux.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css'
+
+const CounterRedux = () => {
+    return (
+        <div className='container'>
+            <h2>Counter // ...</h2>
+            <button>Increment</button>
+            <button>Decrement</button>
+        </div>
+    );
+};
+
+export default CounterRedux;
